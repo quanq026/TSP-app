ed084dbab77875d98413ae959221901c5f3a996e
quanq026 <quantran2k6@gmail.com>
2025-11-29 10:40:16 +0700

    deleted Git_old folder and updated API configuration to use VITE_API_URL environment variable

--- Git/utils/algorithms.ts
+++
@@ -1,5 +0,0 @@
-// Barrel file to export algorithms and utilities
-export * from './geometry';
-export * from './nearestNeighbor';
-export * from './spaceFillingCurve';
-export * from './antColonyOptimization';
